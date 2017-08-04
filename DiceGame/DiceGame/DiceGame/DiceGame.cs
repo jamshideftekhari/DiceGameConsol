9868ee85d6fcff954fbf233bfbd2b8f9fe7ab81c
Jamshid Eftekhari <jaef@easj.dk>
2017-08-04 13:00:52 +0200

    push code to git

---
+++ DiceGame/DiceGame/DiceGame/DiceGame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceGame
+{
+    public class DiceGame
+    {
+        private Die d1;
+        private Die d2;
+        static Random r = new Random();
+        // instance variables - replace the example below with your own
+
+        public DiceGame()
+        {
+            d1 = new Die(r);
+            d2 = new Die(r);
+        }
+
+        public void play()
+        {
+            d1.roll();
+            d2.roll();
+            int faceValue_d1 = d1.getFaceValue();
+            int faceValue_d2 = d2.getFaceValue();
+            int result = faceValue_d1 + faceValue_d2;
+            Console.WriteLine("Result " + result);
+            if (result == 7)
+                Console.WriteLine("You win, congratulation");
+            else
+                Console.WriteLine("You loose, try again");
+
+        }
+
+    }
+
+}
